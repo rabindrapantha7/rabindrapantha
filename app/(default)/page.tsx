b0cf45f4d3577c3ee116a173688a8c96a01ad2e6
Rabindra Pantha <rabindrapantha22@gmail.com>
2023-07-20 20:16:34 +0545

    Update title and description

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: 'Home - Simple',
-  description: 'Page description',
+  title: 'Rabindra Pantha',
+  description: 'Portfolio Website',
 }
 
 import Hero from '@/components/hero'
